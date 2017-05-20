3cd331289845d1dcd5984f432b95050a5958eacb
jpca999 <jpca999@gmail.com>
2017-05-20 12:32:08 -0400

    sendHello function added

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 var express = require ('express');
 
 var app = express();
+var router = express.Router();
+
+var sendHello = function (req,res) {
+  res.send("Hello!");
+}
+router.route('/').get(sendHello);
+
+app.use(router);
+
 
 app.listen(3000, '127.0.0.1', function () {
   console.log(' listing on 3000');
